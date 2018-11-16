e7e219188eae1ccff484b60b787a4415d796645d
shadowfox3735 <normanpan180@gmail.com>
2018-11-16 21:34:50 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,12 +9,12 @@ class School
   end
   
   def add_student(student, grade)
-    @roster[grade]  = []
-    @roster[grade] << student
+    roster[grade]  = []
+    roster[grade] << student
   end
   
   def grade(grade)
-    @roster[grade]
+    roster[grade]
   end
   
 end
